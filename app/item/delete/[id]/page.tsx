9e1db06522d1303a24845b0a7b8c83572b7ed2bd
MikiyaYamaguchi <mikiyagreeeen@gmail.com>
2025-03-21 22:41:53 +0900

    アイテム削除ページ作成

---
+++ app/item/delete/[id]/page.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import React, { use, useEffect, useState } from "react";
+
+const DeleteItem = ({ params }: { params: Promise<{ id: string }> }) => {
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState("");
+  const [image, setImage] = useState("");
+  const [description, setDescription] = useState("");
+  const [email, setEmail] = useState("");
+
+  const router = useRouter();
+
+  const unwrapParams = use(params);
+
+  useEffect(() => {
+    const getSingleItem = async (id: string) => {
+      const response = await fetch(
+        `http://localhost:3001/api/item/readsingle/${id}`,
+        { cache: "no-store" }
+      );
+      const jsonData = await response.json();
+      const singleItem = jsonData.singleItem;
+      setTitle(singleItem.title);
+      setPrice(singleItem.price);
+      setImage(singleItem.image);
+      setDescription(singleItem.description);
+      setEmail(singleItem.email);
+    };
+    getSingleItem(unwrapParams.id);
+  }, [params]);
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(
+        `http://localhost:3001/api/item/delete/${unwrapParams.id}`,
+        {
+          method: "DELETE",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+          body: JSON.stringify({
+            email: "ダミーデータ",
+          }),
+        }
+      );
+      const jsonData = await response.json();
+      alert(jsonData.message);
+      router.push("/");
+      router.refresh();
+    } catch {
+      alert("アイテム削除失敗");
+    }
+  };
+  return (
+    <div>
+      <h1>アイテム削除</h1>
+      <form onSubmit={handleSubmit}>
+        <h2>{title}</h2>
+        <Image src={image} width={750} height={500} alt={title} property="" />
+        <h3>¥{price}</h3>
+        <p>{description}</p>
+        <button>削除</button>
+      </form>
+    </div>
+  );
+};
+
+export default DeleteItem;

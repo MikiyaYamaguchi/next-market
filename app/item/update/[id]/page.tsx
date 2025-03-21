6c36f8c2a734bead6bbf58659e2d3bf30d1b292f
MikiyaYamaguchi <mikiyagreeeen@gmail.com>
2025-03-21 22:35:38 +0900

    アイテム編集ページの作成

---
+++ app/item/update/[id]/page.tsx
@@ -0,0 +1,105 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import React, { use, useEffect, useState } from "react";
+
+const UpdateItem = ({ params }: { params: Promise<{ id: string }> }) => {
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState("");
+  const [image, setImage] = useState("");
+  const [description, setDescription] = useState("");
+  const [email, setEmail] = useState("");
+
+  const router = useRouter();
+
+  const unwrapParams = use(params);
+
+  useEffect(() => {
+    const getSingleItem = async (id: string) => {
+      const response = await fetch(
+        `http://localhost:3001/api/item/readsingle/${id}`,
+        { cache: "no-store" }
+      );
+      const jsonData = await response.json();
+      const singleItem = jsonData.singleItem;
+      setTitle(singleItem.title);
+      setPrice(singleItem.price);
+      setImage(singleItem.image);
+      setDescription(singleItem.description);
+      setEmail(singleItem.email);
+    };
+    getSingleItem(unwrapParams.id);
+  }, [params]);
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(
+        `http://localhost:3001/api/item/update/${unwrapParams.id}`,
+        {
+          method: "PUT",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
+          body: JSON.stringify({
+            title: title,
+            price: price,
+            image: image,
+            description: description,
+            email: "ダミーデータ",
+          }),
+        }
+      );
+      const jsonData = await response.json();
+      alert(jsonData.message);
+      router.push("/");
+      router.refresh();
+    } catch {
+      alert("アイテム編集失敗");
+    }
+  };
+  return (
+    <div>
+      <h1>アイテム編集</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="アイテム名"
+          required
+        />
+        <input
+          type="text"
+          name="price"
+          value={price}
+          onChange={(e) => setPrice(e.target.value)}
+          placeholder="価格"
+          required
+        />
+        <input
+          type="text"
+          name="image"
+          value={image}
+          onChange={(e) => setImage(e.target.value)}
+          placeholder="画像"
+          required
+        />
+        <textarea
+          name="description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          rows={15}
+          placeholder="商品説明"
+          required
+        ></textarea>
+        <button>編集</button>
+      </form>
+    </div>
+  );
+};
+
+export default UpdateItem;

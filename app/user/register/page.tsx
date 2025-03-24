b40f83fd29b0dcde79e091b14fa13f79ceaa7692
MikiyaYamaguchi <mikiyagreeeen@gmail.com>
2025-03-24 22:36:42 +0900

    テスト：クライアントコンポーネントをサーバーコンポーネントに

--- app/user/register/page.tsx
+++ app/user/register/page.tsx
@@ -1,64 +1,7 @@
-"use client";
-import React, { useState } from "react";
+import MyPage from "./myPage";
 
 const Register = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    try {
-      const response = await fetch(
-        `${process.env.NEXT_PUBLIC_URL}/api/user/register`,
-        {
-          method: "POST",
-          headers: {
-            Accept: "application/json",
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            name: name,
-            email: email,
-            password: password,
-          }),
-        }
-      );
-      const jsonData = await response.json();
-      alert(jsonData.message);
-    } catch {
-      alert("ユーザー登録失敗");
-    }
-  };
-  return (
-    <div>
-      <h1 className="page-title">ユーザー登録</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name={name}
-          onChange={(e) => setName(e.target.value)}
-          placeholder="名前"
-          required
-        />
-        <input
-          type="text"
-          name={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="メールアドレス"
-          required
-        />
-        <input
-          type="text"
-          name={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="パスワード"
-          required
-        />
-        <button>登録</button>
-      </form>
-    </div>
-  );
+  return <MyPage />;
 };
 
 export default Register;
